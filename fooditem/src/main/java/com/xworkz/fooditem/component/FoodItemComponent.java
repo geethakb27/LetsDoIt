4797ed83265ea93958e003701329f1fb92934d14
geethakb27 <navyashree.xworkz@gmail.com>
2023-02-03 21:12:29 +0530

    web application running in servlet

---
+++ fooditem/src/main/java/com/xworkz/fooditem/component/FoodItemComponent.java
@@ -0,0 +1,27 @@
+package com.xworkz.fooditem.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@RequestMapping("/tinnu")
+public class FoodItemComponent {
+
+	public FoodItemComponent() {
+		System.out.println("Created" + this.getClass().getSimpleName());
+	}
+
+	@PostMapping
+	public String sending(@RequestParam String foodname, @RequestParam String type, @RequestParam int quantity,
+			@RequestParam double price) {
+		System.out.println("Running sending");
+		System.out.println("Food name:" + foodname);
+		System.out.println("Food type:" + type);
+		System.out.println("Food quantity:" + quantity);
+		System.out.println("Food price:" + price);
+		return "FooditemSucess.jsp";
+		
+	}
+}
