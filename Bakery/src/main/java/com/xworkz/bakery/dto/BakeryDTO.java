0d2ddc08daac12685834af4c2f91a31acf71e48e
geethakb27 <navyashree.xworkz@gmail.com>
2023-02-09 19:27:56 +0530

    web page using button servlets dto

---
+++ Bakery/src/main/java/com/xworkz/bakery/dto/BakeryDTO.java
@@ -0,0 +1,14 @@
+package com.xworkz.bakery.dto;
+
+import lombok.Data;
+
+@Data
+public class BakeryDTO
+{
+	private String name;
+	private String ownerName;
+	private String wifeName;
+	private boolean married;
+	private String famousFor;
+	private int since;
+}
